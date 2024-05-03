928a7f2f0383b107bf609311931a8a319b5ab114
Robin <robin_nygren@outlook.com>
2024-05-03 08:12:13 +0200

    added useFetch

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url: string) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+
+      try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+          throw new Error(
+            `An error occurred while fetching the data: ${response.statusText}`
+          );
+        }
+
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error: any) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+  return { data, loading, error };
+};
+export default useFetch;
