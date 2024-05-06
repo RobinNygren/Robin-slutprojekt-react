aeafb6bbb09953d95f5730e73353664b06d162cc
Robin <robin_nygren@outlook.com>
2024-05-06 14:20:41 +0200

    added useLocation for frontpage

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,17 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../components/Layout/Footer";
 import Header from "../components/Layout/Header";
 import Navbar from "../components/Layout/Navbar";
 import CarouselManager from "../components/CarouselManager/CarouselManager";
 
 const Root = () => {
+  const location = useLocation();
   return (
     <>
       <div className="flex flex-col min-h-screen">
         <Header />
         <Navbar />
         <main className="flex-grow bg-bookFlix-colors-background">
-          <CarouselManager />
+          {location.pathname === "/" && <CarouselManager />}
           <Outlet />
         </main>
         <Footer />
