4a7424dc2eb34055c533c7bcf81c99f58dcb4111
Robin <robin_nygren@outlook.com>
2024-05-06 13:21:31 +0200

    styling BookCard

--- src/components/BookCard/BookCard.tsx
+++ src/components/BookCard/BookCard.tsx
@@ -3,22 +3,33 @@ import { BookCardProps } from "../../types/types";
 const BookCard: React.FC<BookCardProps> = ({ book }) => {
   return (
     <div className="mx-2 flex-none w-30">
-      {book.cover_i ? (
-        <img
-          src={`https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`}
-          alt="Book Cover"
-          className="h-30 w-full object-cover mb-4"
-        />
-      ) : (
-        <p>No cover available</p>
-      )}
-      <h3 className="text-l font-bold text-bookFlix-colors-detail">
-        {book.title}
-      </h3>
-      <p className=" text-bookFlix-colors-detail">Author: {book.author_name}</p>
-      <p className=" text-bookFlix-colors-detail">
-        First Publish Year: {book.first_publish_year}
-      </p>
+      <div
+        className="flex justify-center items-center mb-4"
+        style={{ minHeight: "250px" }}
+      >
+        {book.cover_i ? (
+          <img
+            src={`https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`}
+            alt="Book Cover"
+            className="max-h-60 w-auto object-cover"
+          />
+        ) : (
+          <div className="h-full w-full flex items-center justify-center bg-gray-200">
+            <p>No cover available</p>
+          </div>
+        )}
+      </div>
+      <div className="text-center flex-grow">
+        <h3 className="text-l font-bold text-bookFlix-colors-detail truncate">
+          {book.title}
+        </h3>
+        <p className=" text-bookFlix-colors-detail">
+          Author: {book.author_name}
+        </p>
+        <p className=" text-bookFlix-colors-detail">
+          First Publish Year: {book.first_publish_year}
+        </p>
+      </div>
     </div>
   );
 };
