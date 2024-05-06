bf19f22b3b0a9dcf43ca96b4a4d23f9624bf1285
Robin <Robin_Nygren@outlook.com>
2024-05-06 11:31:37 +0200

    styling

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -25,34 +25,36 @@ const Carousel: React.FC<CarouselProps> = ({ title, children }) => {
   const translateAmount = -(currentIndex * 100);
 
   return (
-    <div className="relative overflow-hidden mx-auto max-w-screen-xl">
+    <div className="relative mx-auto max-w-screen-xl">
       <h1 className="flex justify-center items-center text-2xl font-bold text-bookFlix-colors-secondary">
         {title}
       </h1>
-      <button
-        onClick={prev}
-        aria-label="Previous slide"
-        className="absolute left-0 top-1/2 -translate-y-1/2 z-10 p-2"
-      >
-        <img src={ChevronLeft} alt="Previous" />
-      </button>
-      <div
-        className="flex transition-transform duration-500 ease-out"
-        style={{ transform: `translateX(${translateAmount}%)` }}
-      >
-        {slides.map((slide, index) => (
-          <div key={index} className="flex-none w-full max-w-[20%] p-2">
-            {slide}
-          </div>
-        ))}
+      <div className="relative overflow-hidden">
+        <button
+          onClick={prev}
+          aria-label="Previous slide"
+          className="absolute left-0 top-1/2 -translate-y-1/2 z-10 p-2"
+        >
+          <img src={ChevronLeft} alt="Previous" />
+        </button>
+        <div
+          className="flex transition-transform duration-500 ease-out"
+          style={{ transform: `translateX(${translateAmount}%)` }}
+        >
+          {slides.map((slide, index) => (
+            <div key={index} className="flex-none w-full max-w-[20%] p-2">
+              {slide}
+            </div>
+          ))}
+        </div>
+        <button
+          onClick={next}
+          aria-label="Next slide"
+          className="absolute right-0 top-1/2 -translate-y-1/2 z-10 p-2"
+        >
+          <img src={ChevronRight} alt="Next" />
+        </button>
       </div>
-      <button
-        onClick={next}
-        aria-label="Next slide"
-        className="absolute right-0 top-1/2 -translate-y-1/2 z-10 p-2"
-      >
-        <img src={ChevronRight} alt="Next" />
-      </button>
     </div>
   );
 };
